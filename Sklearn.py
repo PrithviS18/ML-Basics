7b98f719d4f97a6c5c47a7c519fbd3e20015c874
Prithvi Sen <113365172+PrithviS18@users.noreply.github.com>
2024-06-01 20:32:36 +0530

    Create Sklearn.py

---
+++ Sklearn.py
@@ -0,0 +1,15 @@
+!pip install scikit-learn
+
+import sklearn
+# For specific modules, you can import them directly:
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+
+# Or import an entire module
+from sklearn import datasets, model_selection, preprocessing, linear_model, metrics
+
+#the from ... import ... syntax in Python is used to import specific components (such as functions, classes, or variables) from a module or library
+
+
